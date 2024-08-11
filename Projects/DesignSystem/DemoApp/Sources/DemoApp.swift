688ce89c76656945556595ebd7f2ccf5db6fec83
HenryVoid <azicplus@naver.com>
2024-08-11 15:23:45 +0900

    #2 feat: DefaultRadio

--- Projects/DesignSystem/DemoApp/Sources/DemoApp.swift
+++ Projects/DesignSystem/DemoApp/Sources/DemoApp.swift
@@ -1,36 +1,17 @@
 import SwiftUI
 import DesignSystem
 
-
-enum FocusField {
-    case one
-    case two
-    case three
-}
-
 @main
 struct AuthApp: App {
     
     @State var disabled: Bool = false
-    @State var state: CheckBoxState = .unchecked
+    @State var isChecked: Bool = false
     
     var body: some Scene {
         WindowGroup {
             VStack(spacing: 20) {
-                RoundCheckBox(state: state, disabled: false, action: { state in
-                    if state == .unchecked {
-                        self.state = .checked
-                    } else if state == .checked {
-                        self.state = .unchecked
-                    }
-                })
-                
-                DefaultCheckBox(state: state, disabled: false, action: { state in
-                    if state == .unchecked {
-                        self.state = .checked
-                    } else if state == .checked {
-                        self.state = .unchecked
-                    }
+                DefaultRadio(isChecked: isChecked, action: {
+                    isChecked.toggle()
                 })
             }
             .padding(.horizontal, 20)
